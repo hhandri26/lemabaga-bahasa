8d6991729f2a1fee98b8c183e316e472fcc804e4
Muhammad Fari Abiyyudhiya <118955022+fariabyd@users.noreply.github.com>
2025-05-08 20:18:47 +0700

    ubah url api

--- app/src/environments/environment.prod.ts
+++ app/src/environments/environment.prod.ts
@@ -5,6 +5,8 @@ export const environment = {
     // apiUrl: 'https://dev-api-penerjemah.setkab.go.id/v2/api'
     // baseUrl: 'https://dev-api-penerjemah.setkab.go.id/',
     // apiUrl: 'https://dev-api-penerjemah.setkab.go.id/api'
-    baseUrl: 'http://localhost:1000/',
-    apiUrl: 'http://localhost:1000/api'
+    // baseUrl: 'http://localhost:1000/',
+    // apiUrl: 'http://localhost:1000/api'
+    baseUrl: 'https://apilb.kartikaapps.online/',
+    apiUrl: 'https://apilb.kartikaapps.online/api'
 };
--- app/src/environments/environment.ts
+++ app/src/environments/environment.ts
@@ -7,8 +7,10 @@ export const environment = {
     appVersion: 'v2.0',
     // baseUrl: 'https://dev-api-penerjemah.setkab.go.id/',
     // apiUrl: 'https://dev-api-penerjemah.setkab.go.id/api'
-    baseUrl: 'http://localhost:1000/',
-    apiUrl: 'http://localhost:1000/api'
+    // baseUrl: 'http://localhost:1000/',
+    // apiUrl: 'http://localhost:1000/api'
+    baseUrl: 'https://apilb.kartikaapps.online/',
+    apiUrl: 'https://apilb.kartikaapps.online/api'
     // baseUrl: 'https://dev-api-penerjemah.setkab.go.id',
     // apiUrl: 'https://dev-api-penerjemah.setkab.go.id/v2/api'
 };
